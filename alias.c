25dc3ebf16702a8ac89648cd893558f6e8ebb310
abinavpp <abinavpp@gmail.com>
2016-10-04 18:19:08 +0530

    alias_ins simplified, revised api arg list and uses astrcpy

--- alias.c
+++ alias.c
@@ -1,21 +1,17 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "alias.h"
 #include "util.h"
 
 static alias *al_ent[HASH_MAX];
 
-alias *is_alias(char *al_name)
-{
-	return *(al_src(al_name));
-}
-
-alias *al_lin_src(char *al_name, alias *al_head)
+alias *al_lin_src(alias *al_head, char *al_name)
 {
 	alias *walk;
 
-	for (walk=al_head; walk!=NULL && strcmp(walk->name, al_name);)
+	for (walk=al_head; walk && strcmp(walk->name, al_name);)
 		walk=walk->next;
 	return walk;
 }
@@ -24,7 +20,7 @@ void al_lin_free(alias **al_head)
 {
 	alias **walk, **next;
 
-	for (walk=al_head; *walk!=NULL; walk=next) {
+	for (walk=al_head; NOTNULL(walk); walk=next) {
 		next = &((*walk)->next);
 		free((*walk)->name);
 		free(*walk);
@@ -32,41 +28,68 @@ void al_lin_free(alias **al_head)
 	}
 }
 
-void al_lin_ins(char *al_name, alias **al_head)
+void al_lin_ins(alias **al_head, char *al_name)
 {
 	alias **walk;
 
-	for (walk=al_head; *walk!=NULL; )
+	for (walk=al_head; NOTNULL(walk); )
 		walk=&((*walk)->next);
 
-	*walk = (alias *)malloc(sizeof(alias));
-	(*walk)->name = (char *)malloc(strlen(al_name));
-	strcpy((*walk)->name, al_name);
+	if ((*walk = (alias *)malloc(sizeof(alias))) == NULL)
+		ERR_EXIT("malloc");
+	if (( (*walk)->name = (char *)malloc(strlen(al_name)+1) )
+			== NULL)
+		ERR_EXIT("malloc");
+	astrcpy((*walk)->name, al_name, strlen(al_name), 1);
+	(*walk)->next = NULL;
+}
+
+alias *is_alias(char *al_name)
+{
+	return *(al_src(al_name));
 }
 
 alias **al_src(char *al_name)
 {
 	alias **walk;
 
-	for (walk=&al_ent[hash_fun(al_name)]; *walk!=NULL && strcmp((*walk)->name, al_name);)
+	for (walk=&al_ent[hash_fun(al_name)]; 
+			*walk && strcmp((*walk)->name, al_name);)
 		walk = &((*walk)->next);
 	return walk;
 }
 
-void al_ins(alias **al_at, char *al_name, char *al_trans)
+void al_ins(char *al_name, char *al_trans)
 {
+	alias **al_at;
+
+	if (!al_name || !al_trans)
+		return;
+
+	al_at = al_src(al_name);
+
 	if (*al_at == NULL) {
-		*al_at = (alias *)malloc(sizeof(alias));
-		(*al_at)->name = (char *)realloc((*al_at)->name, strlen(al_name));
-		if (al_trans) {
-			(*al_at)->trans = (char *)realloc((*al_at)->trans, strlen(al_trans));
-			strcpy((*al_at)->trans, al_trans);
-		}
-		strcpy((*al_at)->name, al_name);
+		if ((*al_at = (alias *)malloc(sizeof(alias))) == NULL)
+			ERR_EXIT("malloc");
+
+		if (( (*al_at)->name = (char *)malloc(strlen(al_name)+1) )
+				== NULL)
+			ERR("malloc");
+		astrcpy((*al_at)->name, al_name, strlen(al_name), 1);
+
+		if (( (*al_at)->trans = (char *)malloc(strlen(al_trans)+1) )
+				== NULL)
+			ERR_EXIT("malloc");
+
+		astrcpy((*al_at)->trans, al_trans, strlen(al_trans), 1);
+
+		(*al_at)->next = NULL;
 	}
 	else {
-		(*al_at)->trans = (char *)realloc((*al_at)->trans, strlen(al_trans));
-		strcpy((*al_at)->trans, al_trans);
+		if ( ((*al_at)->trans = (char *)realloc
+					((*al_at)->trans, strlen(al_trans)+1)) == NULL)
+			ERR("realloc");
+		astrcpy((*al_at)->trans, al_trans, strlen(al_trans), 1);
 	}
 }
 
@@ -74,6 +97,9 @@ void alias_me(char **cmd)
 {
 	char *al_name, *al_trans;
 	alias *al_cur;
+
+	if (!NOTNULL(cmd[1]))
+		return;
 
 	if (!strchr(cmd[1], '=')) {
 		if ((al_cur=is_alias(cmd[1])))
@@ -85,15 +111,16 @@ void alias_me(char **cmd)
 
 	for (al_name = cmd[1]; *cmd[1]!='\0' && *cmd[1]!='=';) {
 		if (*cmd[1] == ' ')
-			*cmd[1] = '\0'; /* eat em bloody spaces my baby */
+			*cmd[1] = '\0'; /* eat em bloody spaces */
 		cmd[1]++;
 	}
 	*cmd[1] = '\0'; /* al_name points till here */
 	while (*(++cmd[1]) == ' ')
 		; /* skip em bloody spaces made by puny humans */
 
-	al_trans = cmd[1]; /* trailing spaces will be taken care by the parser */
+	/* trailing spaces will be taken care by the parser */
+	al_trans = cmd[1]; 
 
-	al_ins(al_src(al_name), al_name, al_trans); /* alias is hashed in */
+	al_ins(al_name, al_trans); /* alias is hashed in */
 	printf("aliasing %s with %s\n", al_name, al_trans);
  }
--- alias.h
+++ alias.h
@@ -9,13 +9,13 @@ typedef struct alias {
 
 #define ALIAS_DIFF(al)	(strlen((al)->trans) - strlen((al)->name))
 
-extern void al_lin_ins(char *al_name, alias **al_at);
+extern void al_lin_ins(alias **al_at, char *al_name);
 extern void al_lin_free(alias **al_head);
-extern alias *al_lin_src(char *al_name, alias *al_head);
+extern alias *al_lin_src(alias *al_head, char *al_name);
 
 extern alias *is_alias(char *al_name);
 extern alias **al_src(char *al_name);
-extern void al_ins(alias **al_at, char *al_name, char *al_trans);
+extern void al_ins(char *al_name, char *al_trans);
 extern void alias_me(char **cmd);
 
 #endif
